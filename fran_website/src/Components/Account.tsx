17da237ffdcc132f9a7c29a9ac6e978b40a41fa8
Nicholas McNamara <nikomcnamara@gmail.com>
2024-08-29 14:02:25 -0400

    Account Page component outline

---
+++ fran_website/src/Components/Account.tsx
@@ -0,0 +1,11 @@
+
+interface AccountProps {
+    props: null;
+}
+const Account: React.FC<AccountProps> = () => {
+    return (
+        <div className="account-container"></div>
+    )
+}
+
+export default Account;

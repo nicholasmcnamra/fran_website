4a05237d8676d7ce94ca04b67f42703f94775478
Nicholas McNamara <nikomcnamara@gmail.com>
2024-09-05 12:50:11 -0400

    adding aws dependency

---
+++ fran_website/java~springbootApp/FranArtistSite_springboot/src/main/java/com/francarter/FranArtistSite/Controllers/BucketController.java
@@ -0,0 +1,10 @@
+package com.francarter.FranArtistSite.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/storage")
+public class BucketController {
+
+}

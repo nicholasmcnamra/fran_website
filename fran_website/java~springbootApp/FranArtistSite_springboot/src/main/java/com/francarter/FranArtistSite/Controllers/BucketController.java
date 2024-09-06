52b0e0061cac257deb8590ed3693efb06c1ff8fd
Nicholas McNamara <nikomcnamara@gmail.com>
2024-09-06 16:45:32 -0400

    service and controller

--- fran_website/java~springbootApp/FranArtistSite_springboot/src/main/java/com/francarter/FranArtistSite/Controllers/BucketController.java
+++ fran_website/java~springbootApp/FranArtistSite_springboot/src/main/java/com/francarter/FranArtistSite/Controllers/BucketController.java
@@ -1,10 +1,24 @@
 package com.francarter.FranArtistSite.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.francarter.FranArtistSite.Services.AmazonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/storage")
 public class BucketController {
-
+    private AmazonClient amazonClient;
+    @Autowired
+    BucketController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file")MultipartFile file) {
+        return this.amazonClient.uploadFile(file);
+    }
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value="url") String fileUrl) {
+        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+    }
 }
---
+++ fran_website/java~springbootApp/FranArtistSite_springboot/src/main/java/com/francarter/FranArtistSite/Services/AmazonClient.java
@@ -0,0 +1,65 @@
+package com.francarter.FranArtistSite.Services;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+@Service
+public class AmazonClient {
+    private AmazonS3 s3client;
+    @Value("${amazon.s3.endpointUrl}")
+    private String endpointUrl;
+    @Value("${amazon.s3.bucketName}")
+    private String bucketName;
+    @Value("${amazon.s3.accessKey}")
+    private String accessKey;
+    @Value("${amazon.s3.secretKey}")
+    private String secretKey;
+
+    private void initializeAmazon() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+    }
+    private File convertMultipartFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream outputStream = new FileOutputStream(convFile);
+        outputStream.write(file.getBytes());
+        outputStream.close();
+        return convFile;
+    }
+    private String generateFileName(MultipartFile multipartFile) {
+        return new Date().getTime() + "-" + multipartFile.getOriginalFilename().replace(" ", "_");
+    }
+    private void uploadFileToS3Bucket(String fileName, File file) {
+        s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+    public String uploadFile(MultipartFile multipartFile) {
+        String fileUrl = "";
+        try {
+            File file = convertMultipartFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+            fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
+            uploadFileToS3Bucket(fileName, file);
+            file.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileUrl;
+    }
+    public String deleteFileFromS3Bucket( String fileUrl) {
+        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/"));
+        s3client.deleteObject((new DeleteObjectRequest(bucketName + "/", fileName)));
+        return "Successfully deleted";
+    }
+}

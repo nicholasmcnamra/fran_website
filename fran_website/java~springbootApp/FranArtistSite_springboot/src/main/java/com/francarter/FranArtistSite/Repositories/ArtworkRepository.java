9601255241a4a83df349c798cef090645c5b7325
Nicholas McNamara <nikomcnamara@gmail.com>
2025-06-05 14:22:42 -0400

    restructure directories

--- fran_website/java~springbootApp/FranArtistSite_springboot/src/main/java/com/francarter/FranArtistSite/Repositories/ArtworkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.francarter.FranArtistSite.Repositories;
-
-import com.francarter.FranArtistSite.Entities.Artwork;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArtworkRepository extends CrudRepository<Artwork, Long> {
-}
